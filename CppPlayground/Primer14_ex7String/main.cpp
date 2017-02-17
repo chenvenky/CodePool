b66de6738b381cf0023dce836037f3a414c30482
chenwenke <2568333286@qq.com>
2017-02-17 16:28:06 +0800

    重载输出运算符

---
+++ CppPlayground/Primer14_ex7String/main.cpp
@@ -0,0 +1,9 @@
+#include "String.h"
+using namespace std;
+
+int main()
+{
+    String str("Never let the truth in the way of a good story.");
+    cout << str << endl;
+    return 0;
+}
---
+++ CppPlayground/Primer14_ex7String/src/String.cpp
@@ -0,0 +1,61 @@
+#include "String.h"
+#include <algorithm>
+#include <iostream>
+
+std::pair<char*, char*>
+String::alloc_n_copy(const char*b, const char *e)
+{
+    auto str = alloc.allocate(e - b);
+    return { str, std::uninitialized_copy(b, e, str)};
+}
+
+void String::range_initializer(const char *first, const char *last)
+{
+    auto newstr = alloc_n_copy(first, last);
+    elements = newstr.first;
+    end = newstr.second;
+}
+
+String::String(const char *s)
+{
+    char *s1 = const_cast<char*>(s);
+    while (*s1)
+        ++s1;
+    range_initializer(s, ++s1);
+}
+
+String::String(const String &rhs)
+{
+    range_initializer(rhs.elements, rhs.end);
+}
+
+void String::free()
+{
+    if (elements)   // 检查一下，以防重复释放内存
+    {
+        std::for_each(elements, end, [this](char &c) { alloc.destroy(&c); });
+        alloc.deallocate(elements, end - elements);
+    }
+}
+
+String::~String()
+{
+    free();
+}
+
+String& String::operator=(const String &rhs)
+{
+    auto newstr = alloc_n_copy(rhs.elements, rhs.end);
+    free();
+    elements = newstr.first;
+    end = newstr.second;
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream &os, const String &s)
+{
+    char *c = const_cast<char*>(s.c_str());
+    while (*c)
+        os << *c++;
+    return os;
+}
