87fbaba3f62095c16f0fbe13d1462207da2e8326
chenwenke <2568333286@qq.com>
2017-02-19 21:21:49 +0800

    简单排列 - 注意细节

---
+++ LeetCode/17.LetterCombinationsOfAPhoneNumber/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> letterCombinations(string digits)
+{
+   vector<string> result;
+    if(digits.empty()) return vector<string>();
+    static const vector<string> v = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    result.push_back("");   // add a seed for the initial case
+    for(int i = 0 ; i < digits.size(); ++i) {
+        int num = digits[i]-'0';
+        if(num < 0 || num > 9) break;
+        const string& candidate = v[num];
+        if(candidate.empty()) continue;
+        vector<string> tmp;
+        for(int j = 0 ; j < candidate.size() ; ++j) {
+            for(int k = 0 ; k < result.size() ; ++k) {
+                tmp.push_back(result[k] + candidate[j]);
+            }
+        }
+        result.swap(tmp);
+    }
+    return result;
+}
+int main()
+{
+    vector<string> ans = letterCombinations("23");
+    for (auto &i : ans)
+        cout << i << " ";
+    cout << endl;
+    return 0;
+}
