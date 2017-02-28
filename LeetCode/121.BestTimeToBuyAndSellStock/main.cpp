1869133f6259fb7b75623d60d0db2c9049faf2ee
chenwenke <2568333286@qq.com>
2017-02-28 21:18:01 +0800

    简单题

---
+++ LeetCode/121.BestTimeToBuyAndSellStock/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int maxProfit(vector<int> &prices){
+    int maxPro = 0;
+    int minPrice = INT_MAX;
+    for (int i = 0; i < prices.size(); i++)
+    {
+        minPrice = min(minPrice, prices[i]);
+        maxPro = max(maxPro, prices[i] - minPrice);
+    }
+    return maxPro;
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
