068f119a8cba6258540147fcedd9151d080aec41
ChenWenKe <chenwenke666@gmail.com>
2017-07-10 00:44:06 -0700

    向服务器发送记录类 --- 功能已测试

--- Project/ServerRentSystem/client/SocketSender.cpp
+++ Project/ServerRentSystem/client/SocketSender.cpp
@@ -2,28 +2,53 @@
 
 
 // 构造函数
-SocketSender::SocketSender(std::string const& failFile, std::string const& ip, const short port, int sockfd)
+SocketSender::SocketSender(std::string const& failFile, std::string const& ip, const short port):m_failFile(failFile), m_ip(ip), m_port(port)
 {
-   // TODO : implement
+    sockfd = -1; 
 }
 
 // 析构函数 -- 关闭 TCP 连接
 SocketSender::~SocketSender()
 {
-   // TODO : implement
+    close(sockfd); 
 }
 
+
 // 通过网络发送日志文件
+// 1. 建立连接
+// 2. 读取上次失败的日志记录
+// 3. 通过网络发送日志记录给服务器
+// 4. 保存发送失败的日志记录
 void SocketSender::sendLog(list<MLogRec>& logs)
 {
-   // TODO : implement
+    connectServer(); 
+    readFailFile(logs); 
+    sendData(logs); 
 }
 
 
 // 连接服务器
-void SocketSender::connectSever(void)
+void SocketSender::connectServer(void)
 {
-   // TODO : implement
+   // 1. 获取 socket 描述符
+   sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+   if(sockfd == -1) 
+   {
+       // throw err
+   }
+
+   // 2. 准备通信地址
+   struct sockaddr_in server_addr; 
+   server_addr.sin_family = AF_INET;    // 使用 ipv4地址
+   server_addr.sin_addr.s_addr = inet_addr(m_ip.c_str()); 
+   server_addr.sin_port = htons(m_port); 
+
+   // 3. 连接服务器
+   int ret = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)); 
+   if(ret == -1)
+   {
+       // throw err strerror(errno)
+   }
 }
 
 
@@ -31,19 +56,92 @@ void SocketSender::connectSever(void)
 // 读取上次发送失败的日志文件记录
 void SocketSender::readFailFile(list<MLogRec>& logs)
 {
-   // TODO : implement
+    ifstream ifs(m_failFile); 
+    if(!ifs)
+    {
+        // throw ReadException("File: " + m_failFile" + "can't open"); 
+    }
+
+    MLogRec mrec; 
+    while(ifs.peek() != EOF)
+    {
+        ifs >> mrec.logname >> mrec.logintime >> 
+            mrec.logouttime >> mrec.durations >> mrec.logip;
+
+        logs.push_back(mrec); 
+    }
+
+    ifs.close(); 
 }
 
 
 // 通过网络将容器中的数据发送给服务器
 void SocketSender::sendData(list<MLogRec>& logs)
 {
-   // TODO : implement
+     char buf[1024]; 
+
+     while(logs.size())
+     {
+        MLogRec mrec = logs.front();  
+        logs.pop_front();  
+        
+        strcpy(buf, "");         
+        sprintf(buf, "%s %s", buf, mrec.logname); 
+        sprintf(buf, "%s %d", buf, mrec.logintime); 
+        sprintf(buf, "%s %d", buf, mrec.logouttime); 
+        sprintf(buf, "%s %d", buf, mrec.durations); 
+        sprintf(buf, "%s %s", buf, mrec.logip);     
+        int ret = send(sockfd, buf, strlen(buf), 0); 
+        
+        if(ret == -1)
+        {
+            saveFailFile(logs); 
+            break; 
+        }
+     }
 }
 
 
 // 保存发送失败的日志记录
 void SocketSender::saveFailFile(list<MLogRec>& log)
 {
-   // TODO : implement
+    ofstream ofs(m_failFile); 
+    if(!ofs)
+    {
+        // throw SaveException("Can't save File" + m_loginsFile);
+    }
+
+    // Note: the format 
+    for(list<MLogRec>::iterator it = log.begin(); it != log.end(); it++)
+    {
+        ofs << it->logname << " " << it->logintime << " " << it->logouttime << " "
+            << it->durations << " " << it->logip << endl; 
+    }
+        
+    ofs.close(); 
 }
+
+// Test for debug
+/*
+int main()
+{
+    string failFile = "failFile"; 
+    string Ip = "127.0.1.1"; 
+    int port = 8000; 
+
+    SocketSender sender(failFile, Ip, port); 
+    list<MLogRec> log;
+     
+    MLogRec mrec; 
+    strcpy(mrec.logname, "chenwenke"); 
+    mrec.logintime = 20170710; 
+    mrec.logouttime = 20170711; 
+    mrec.durations = 1; 
+    strcpy(mrec.logip, "192.168.0.1"); 
+    log.push_back(mrec); 
+
+    sender.sendLog(log); 
+
+    return 0; 
+}
+*/
--- Project/ServerRentSystem/client/SocketSender.h
+++ Project/ServerRentSystem/client/SocketSender.h
@@ -7,22 +7,20 @@
 class SocketSender : public LogSender
 {
 public:
-   SocketSender(std::string const& failFile, std::string const& ip, const short port, int sockfd);
+   SocketSender(std::string const& failFile, std::string const& ip, const short port);
    ~SocketSender();
    virtual void sendLog(list<MLogRec>& logs);   // 通过网络发送日志记录
 
 private:
-   void connectSever(void);                     // 连接服务器
+   void connectServer(void);                     // 连接服务器
    void readFailFile(list<MLogRec>& logs);      // 读取上次失败的日志记录
    void sendData(list<MLogRec>& logs);          // 通过网络发送日志记录给服务器
    void saveFailFile(list<MLogRec>& log);       // 保存发送失败的日志记录
 
-   std::string m_failFile;      // 上次发送失败的已匹配记录的文件
-   std::string m_ip;            // 服务器进程 IP 
-   std::string m_port;          // 服务器进程端口
+   const std::string m_failFile;      // 上次发送失败的已匹配记录的文件
+   const std::string m_ip;            // 服务器进程 IP 
+   const short m_port;          // 服务器进程端口
    int sockfd;                  // 套接字描述符
-
-
 };
 
 #endif  // __SOCKETSENDER_H__
