3518ce56a15b1b2a9a086cf67ffb4e8c107b9db8
ChenWenKe <chenwenke666@gmail.com>
2017-07-10 00:42:45 -0700

    发送日志 -- 抽象基类

--- Project/ServerRentSystem/client/LogSender.cpp
+++ Project/ServerRentSystem/client/LogSender.cpp
@@ -1,17 +1,7 @@
 #include "LogSender.h"
 
-
-LogSender::LogSender()
-{
-}
-
 LogSender::~LogSender()
 {
-   // TODO : implement
+
 }
 
-// 定义发送日志记录的接口
-void LogSender::sendLog(list<MLogRec>& logs)
-{
-   // TODO : implement
-}
--- Project/ServerRentSystem/client/LogSender.h
+++ Project/ServerRentSystem/client/LogSender.h
@@ -1,8 +1,6 @@
 #ifndef __LOGSENDER_H__
 #define __LOGSENDER_H__
 
-#include <iostream>
-#include <list>
 #include "data.h"
 
 using namespace std;
@@ -10,7 +8,6 @@ using namespace std;
 class LogSender
 {
 public:
-   LogSender();
    virtual ~LogSender();
    virtual void sendLog(list<MLogRec>& logs) = 0;   // 定义发送日志记录的接口
 
