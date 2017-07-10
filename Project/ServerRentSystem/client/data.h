819e0dd8f4b157b0b7af3e94c99b90da07269c6d
ChenWenKe <chenwenke666@gmail.com>
2017-07-10 00:44:21 -0700

    添加 头文件

--- Project/ServerRentSystem/client/data.h
+++ Project/ServerRentSystem/client/data.h
@@ -2,15 +2,32 @@
 #define _DATA_H_
 
 #include "ClientException.h"
-#include <list>
+#include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <fstream>
 #include <arpa/inet.h>
+#include <signal.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+#include <sys/mman.h>
+
+#include <iostream>
+#include <fstream>
+#include <list>
+
+
+#define MAXLINE     1024    // Max text line length 
+#define MAXBUF      8192    // Max I/O buffer size
+#define LISTENQ     1024    // Second argument to listen()
 
 typedef struct
 {
@@ -30,5 +47,8 @@ typedef struct
 	char logip[257];
 }MLogRec;
 
-#endif //_DATA_H_
 
+// External variables
+extern char **environ;      // Defined by libc
+
+#endif //_DATA_H_
