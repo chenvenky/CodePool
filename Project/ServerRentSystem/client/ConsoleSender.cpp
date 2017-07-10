fd4f5b7d6c1c1c235103a7b21f26a2b1a15eb486
ChenWenKe <chenwenke666@gmail.com>
2017-07-10 00:39:59 -0700

    把匹配记录输出到屏幕类

--- Project/ServerRentSystem/client/ConsoleSender.cpp
+++ Project/ServerRentSystem/client/ConsoleSender.cpp
@@ -8,12 +8,20 @@ ConsoleSender::ConsoleSender()
 
 ConsoleSender::~ConsoleSender()
 {
-   // TODO : implement
+ 
 }
 
 
 // 将已匹配的日志记录输出到控制台
 void ConsoleSender::sendLog(list<MLogRec>& logs)
 {
-   // TODO : implement
+    list<MLogRec>::iterator it = logs.begin(); 
+    while(it != logs.end())
+    {
+        cout << it->logname << "\t" << it->logintime << 
+        "\t" << it->logouttime << "\t" << it->durations << 
+        "\t" << it->logip << endl; 
+
+        it++; 
+    }
 }
