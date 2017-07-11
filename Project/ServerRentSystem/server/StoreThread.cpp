ebb8be8d1bfb3fdd5dfbf28b7b218430a31fc8ec
ChenWenKe <chenwenke666@gmail.com>
2017-07-11 05:18:08 -0700

    待补充

--- Project/ServerRentSystem/server/StoreThread.cpp
+++ Project/ServerRentSystem/server/StoreThread.cpp
@@ -7,7 +7,7 @@ StoreThread::StoreThread(LogDao &dao) : m_dao(dao)
 
 StoreThread::~StoreThread()
 {
-   // TODO : implement
+
 }
 
 // 从存储队列中取出数据，交给 dao
@@ -27,7 +27,11 @@ void StoreThread::run(void)
         printf("%d\t", storeRec.logouttime); 
         printf("%d\t", storeRec.durations);
         printf("%s\n", storeRec.logip); 
-        // TODO: 存入数据库
+        // TODO: 存入数据库或数据库文件
+
+        // 存入数据库文件
+        m_dao.insert(storeRec); 
+
     }
 
    delete this; 
