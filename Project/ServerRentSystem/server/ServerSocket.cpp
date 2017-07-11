aa8b98385ed4b1c0b10dba9650a42ba17565ed14
ChenWenKe <chenwenke666@gmail.com>
2017-07-10 19:19:27 -0700

    代码完善中

--- Project/ServerRentSystem/server/ServerSocket.cpp
+++ Project/ServerRentSystem/server/ServerSocket.cpp
@@ -3,13 +3,36 @@
 // 启动套接字监听线程
 ServerSocket::ServerSocket(std::string const& ip, short port)
 {
-   // TODO : implement
+    m_sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if(m_sockfd == -1)
+    {
+        // printf(), strerror(errno); 
+    }
+
+    struct sockaddr_in server_addr; 
+    server_addr.sin_family = AF_INET;   // 使用 ipv4 地址
+    
+    server_addr.sin_addr.s_addr = inet_addr(ip.c_str());   // 具体的 ip 地址 
+
+    server_addr.sin_port = htons(port);
+
+    // 3. bind 通信地址
+    int ret = bind(m_sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)); 
+    if(ret == -1)
+    {
+        // printf(""); strerror(errno);
+    }
+
+    // 4. 监听
+    listen(m_sockfd, 1024); 
+     
 }
+
 
 
 ServerSocket::~ServerSocket()
 {
-   // TODO : implement
+   close(m_sockfd); 
 }
 
 /*
@@ -19,5 +42,16 @@ ServerSocket::~ServerSocket()
  */
 void ServerSocket::acceptClient(void)
 {
-   // TODO : implement
+    while(1)
+    {
+        struct sockaddr_in client_addr; 
+        socklen_t len = sizeof(client_addr); 
+        int accfd = accept(m_sockfd, (struct sockaddr*)&client_addr, &len); 
+        if(accfd == -1)
+        {
+            // printf("server accept failed")
+            return; 
+        }
+        (new ClientThread(accfd))->start(); 
+    }
 }
