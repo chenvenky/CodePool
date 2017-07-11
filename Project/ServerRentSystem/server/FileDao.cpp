df4fda9c994a9ad1e1a6f1a2696f651471eef89b
ChenWenKe <chenwenke666@gmail.com>
2017-07-11 02:53:09 -0700

    文件Z

--- Project/ServerRentSystem/server/FileDao.cpp
+++ Project/ServerRentSystem/server/FileDao.cpp
@@ -1,21 +1,23 @@
 #include "FileDao.h"
 
 // 初始化 文件输出流
-FileDao::FileDao(std::string const& dbFile)
+FileDao::FileDao(std::string const& dbFile) : m_ofs(dbFile.c_str()) 
 {
-   // TODO : implement
+    if(!m_ofs)
+        throw FileException("把数据保存到 数据库文件时： 文件 " + dbFile + "无法打开"); 
 }
 
 
 // 关闭 文件输出流
 FileDao::~FileDao()
 {
-   // TODO : implement
+    m_ofs.close(); 
 }
 
 
 // 把数据写入数据文件
 void FileDao::insert(MLogRec const& log)
 {
-   // TODO : implement
+    m_ofs   << log.logname << " " << log.logintime << " " << log.logouttime << " "
+            << log.durations << " " << log.logip << endl; 
 }
