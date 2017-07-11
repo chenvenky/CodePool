5a2b32168cf58f11e992caf50b840fa98f6f0343
ChenWenKe <chenwenke666@gmail.com>
2017-07-11 05:16:50 -0700

    已完善

--- Project/ServerRentSystem/server/Server.cpp
+++ Project/ServerRentSystem/server/Server.cpp
@@ -1,24 +1,26 @@
 #include "Server.h"
 
 
-Server::Server(LogDao& dao, std::string const& ip, const short port):m_socket(ip, port),
-    m_store(dao)
+Server::Server(LogDao& dao, std::string const& ip, const short port):m_socket(ip, port), m_store(dao)
 {
-   // TODO : implement
+
 }
 
 
 Server::~Server()
 {
-   // TODO : implement
+
 }
 
 /*
  * 启动服务器端数据采集
- * (1). 启动接收客户端发送数据的线程
- * (2). 启动数据存储的线程（注意同步）
+ * (1). 启动数据存储的线程（注意同步）
+ * (2). 启动接收客户端发送数据的线程
  */ 
-void Server::dataMine(void)
+void Server::dataMine(void) throw(ServerException)
 {
-   // TODO : implement
+
+    m_store.start(); 
+    printf("数据存储线程已启动!\n"); 
+    m_socket.acceptClient();  
 }
