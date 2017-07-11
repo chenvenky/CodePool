8f1b0d5a3bdef8f41fa0f637b0c4995070daa46a
ChenWenKe <chenwenke666@gmail.com>
2017-07-10 22:22:32 -0700

    功能已完善

--- Project/ServerRentSystem/server/ClientThread.cpp
+++ Project/ServerRentSystem/server/ClientThread.cpp
@@ -9,34 +9,47 @@ ClientThread::ClientThread(int accfd) : m_accfd(accfd)
 
 ClientThread::~ClientThread()
 {
-   // TODO : implement
+
 }
 
 // 接收客户端数据的线程处理函数, 并把数据放入存储队列
 void ClientThread::run(void)
 {
-    pthread_detach(pthread_self());     // detach thread
     
+    extern LogQueue logQu; 
     char buf[1024]; 
     int count = 0; 
+
+    list<MLogRec> logList; 
     while(recv(m_accfd, buf, sizeof(buf), 0))
     {
 //        printf("%s\n", buf);  
-    	MLogRec mrec2; 
+    	MLogRec mrec; 
           
-    	sscanf(buf, "%s %d %d %d %s", mrec2.logname, &mrec2.logintime, &mrec2.logouttime, &mrec2.durations, mrec2.logip); 
-    
-        printf("%s\t", mrec2.logname); 
-        printf("%d\t", mrec2.logintime); 
-        printf("%d\t", mrec2.logouttime); 
-        printf("%d\t", mrec2.durations); 
-        printf("%s\n", mrec2.logip); 
+    	sscanf(buf, "%s %d %d %d %s", mrec.logname, &mrec.logintime, &mrec.logouttime, &mrec.durations, mrec.logip); 
+        strcpy(buf, ""); 
+
+        logList.push_back(mrec); 
+        
+/*      
+        printf("%s\t", mrec.logname); 
+        printf("%d\t", mrec.logintime); 
+        printf("%d\t", mrec.logouttime); 
+        printf("%d\t", mrec.durations); 
+        printf("%s\n", mrec.logip); 
 
         strcpy(buf, ""); 
         count++; 
+*/
     }
 
-    printf("Total:\t%d\n", count); 
+    while(logList.size())
+    {
+        logQu << logList.front(); 
+        logList.pop_front(); 
+    }
+
+//    printf("Total:\t%d\n", count); 
 
 
     delete this; 
