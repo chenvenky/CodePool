4821796f530490c6f950b532b2b9eee1d31d4787
ChenWenKe <chenwenke666@gmail.com>
2017-07-11 21:42:16 -0700

    注释掉 测试代码

--- Project/ServerRentSystem/server/StoreThread.cpp
+++ Project/ServerRentSystem/server/StoreThread.cpp
@@ -21,12 +21,13 @@ void StoreThread::run(void)
     while(1)
     {
         logQu >> storeRec; 
-
+/*
         printf("%s\t", storeRec.logname); 
         printf("%d\t", storeRec.logintime); 
         printf("%d\t", storeRec.logouttime); 
         printf("%d\t", storeRec.durations);
         printf("%s\n", storeRec.logip); 
+*/
         // TODO: 存入数据库或数据库文件
 
         // 存入数据库文件
