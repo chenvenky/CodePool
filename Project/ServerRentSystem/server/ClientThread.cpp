54fa2334068d04504c34f8f9e909d7e6058411d5
ChenWenKe <chenwenke666@gmail.com>
2017-07-13 04:13:31 -0700

    批量接收 客户端发送来的数据

--- Project/ServerRentSystem/server/ClientThread.cpp
+++ Project/ServerRentSystem/server/ClientThread.cpp
@@ -15,30 +15,34 @@ ClientThread::~ClientThread()
 // 接收客户端数据的线程处理函数, 并把数据放入存储队列
 void ClientThread::run(void)
 {
-    
     extern LogQueue logQu; 
-
     list<MLogRec> logList; 
     MLogRec mrec; 
+	const int BUFSIZE = 2048;   
+	char buf[BUFSIZE];          // Note: buf[] can't be global/static variable --- thread safe
+	int rlen = 0; 
+	int pos = 0; 
+	int logLen = sizeof(mrec); 
+	while((rlen = recv(m_accfd, buf, BUFSIZE, 0)) > 0)
+	{
+		int pos = 0; 
+		while(rlen >= logLen) 
+		{
+			memcpy((char*)(&mrec) + (sizeof(mrec) - logLen), buf + pos, logLen); 
+			pos += logLen; 
+			rlen -= logLen; 
+			
+			logList.push_back(mrec); 
+			logLen = sizeof(mrec); 
+		}
 
-    while(1)
-    {
-        int len = sizeof(mrec); 
-        int rlen = 0; 
-        while(len)
-        {
-            rlen = recv(m_accfd, (char*)(&mrec) + (sizeof(mrec) - len), len, 0); 
-            
-            if(rlen <= 0)       // end or err
-                goto label; 
+		if(rlen < logLen)
+		{
+			memcpy((char*)(&mrec) + (sizeof(mrec) - logLen), buf + pos, rlen); 
+			logLen -= rlen; 
+		}
+	}
 
-            len -= rlen; 
-        }
-
-        logList.push_back(mrec); 
-    }
-    
-label:
     cout << "Total records: " << logList.size() << endl; 
 
     while(logList.size())
